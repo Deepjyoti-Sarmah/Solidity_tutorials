a621fce067379debefd59f4d7df82c1306ed1358
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-07-14 16:57:51 +0530

    testing raffle

--- foundry-f23/foundry-smart-contract-lottery-f23/test/unit/RaffleTest.t.sol
+++ foundry-f23/foundry-smart-contract-lottery-f23/test/unit/RaffleTest.t.sol
@@ -32,9 +32,29 @@ contract RaffleTest is Test {
             callbackGasLimit,
             vrfCoordinatorV2
         ) = helperConfig.activeNetworkConfig();
+        vm.deal(PLAYER, STARTING_USER_BALANCE);
     }
 
     function testRaggleInitializesInOpenState() public view {
         assert(raffle.getRaffleState() == Raffle.RaffleState.OPEN);
+    }
+
+    ///////////////////
+    // enterRaffle   //
+    ///////////////////
+
+    function testRaffleRevertsWhenYouDontPayEnough() public {
+        //Arrange
+        vm.prank(PLAYER);
+        //Act // Assert
+        vm.expectRevert(Raffle.Raffle__SendMoreToEnterRaffle.selector);
+        raffle.enterRaffle();
+    }
+
+    function testRaffleRecordsPlayerWhenTheyEnter() public {
+        vm.prank(PLAYER);
+        raffle.enterRaffle{value: entranceFee}();
+        address playerRecorded = raffle.getPlayer(0);
+        assert(playerRecorded == PLAYER);
     }
 }
