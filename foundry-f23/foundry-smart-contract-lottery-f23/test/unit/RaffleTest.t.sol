859cd29c6954a1e3e9434f060b7f8ae33e39c94f
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-07-17 16:53:11 +0530

    adding test coverage file and testing checkupkeep

--- foundry-f23/foundry-smart-contract-lottery-f23/test/unit/RaffleTest.t.sol
+++ foundry-f23/foundry-smart-contract-lottery-f23/test/unit/RaffleTest.t.sol
@@ -81,4 +81,40 @@ contract RaffleTest is Test {
         vm.prank(PLAYER);
         raffle.enterRaffle{value: entranceFee}();
     }
+
+    //////////////////
+    // checkupKeeep///
+    //////////////////
+
+    function testCheckUpkeepReturnsFalseIfItHasNoBalance() public {
+        //Arrange
+        vm.warp(block.timestamp + interval + 1);
+        vm.roll(block.number + 1);
+
+        //Act
+        (bool upkeepNeeded, ) = raffle.checkUpkeep("");
+
+        //Assert
+        assert(!upkeepNeeded);
+    }
+
+    function testCheckUpKeepReturnsFalseIfRaffleNotOpen() public {
+        //Arrange
+        vm.prank(PLAYER);
+        raffle.enterRaffle{value: entranceFee}();
+        vm.warp(block.timestamp + interval + 1);
+        vm.roll(block.number + 1);
+        raffle.performUpkeep("");
+
+        //Act
+        (bool upKeepNeeded, ) = raffle.checkUpkeep("");
+        assert(upKeepNeeded == false);
+    }
+
+    // testCheckUpKeepReturnsFalseIfEnoughTimeHasntPassed
+    // testCheckUpkeepReturnsTrueWhenParametersAreGood
+
+    /////////////////////
+    // performUpkeep/////
+    /////////////////////
 }
