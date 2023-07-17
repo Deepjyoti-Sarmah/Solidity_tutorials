ee239246d2cfd7c2396683db6a893ead56f81ed2
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-07-17 16:14:14 +0530

    added foundry-devops to add consumer script and feed fee to the sunscription

--- foundry-f23/foundry-smart-contract-lottery-f23/script/Interactions.s.sol
+++ foundry-f23/foundry-smart-contract-lottery-f23/script/Interactions.s.sol
@@ -6,6 +6,7 @@ import {Script, console} from "forge-std/Script.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
 import {VRFCoordinatorV2Mock} from "@chainlink/contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol";
 import {LinkToken} from "../test/mocks/LinkToken.sol";
+import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
 
 contract CreateSubsription is Script {
     function createSubscriptionUsingConfig() public returns (uint64) {
@@ -76,5 +77,36 @@ contract FundSubscription is Script {
 
     function run() external {
         fundSubscriptionConfig();
+    }
+}
+
+contract AddConsumer is Script {
+    function addConsumer(
+        address raffle,
+        address vrfCoordinator,
+        uint64 subId
+    ) public {
+        console.log("Adding consumer contract:", raffle);
+        console.log("Using vrfCoordinator:", vrfCoordinator);
+        console.log("On ChainID:", block.chainid);
+        vm.startBroadcast();
+        VRFCoordinatorV2Mock(vrfCoordinator).addConsumer(subId, raffle);
+        vm.stopBroadcast();
+    }
+
+    function addConsumerUsingConfig(address raffle) public {
+        HelperConfig helperConfig = new HelperConfig();
+
+        (uint64 subId, , , , , address vrfCoordinatorV2, ) = helperConfig
+            .activeNetworkConfig();
+        addConsumer(raffle, vrfCoordinatorV2, subId);
+    }
+
+    function run() external {
+        address raffle = DevOpsTools.get_most_recent_deployment(
+            "Raffle",
+            block.chainid
+        );
+        addConsumerUsingConfig(raffle);
     }
 }
