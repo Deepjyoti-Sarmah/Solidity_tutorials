7b8bb2365f034dfff0cbbd08fb2727f4f90fb8ad
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-07-22 13:10:03 +0530

    setting up interactiontest.t.sol

--- foundry-f23/foundry-smart-contract-lottery-f23/test/integration/InteractionsTest.t.sol
+++ foundry-f23/foundry-smart-contract-lottery-f23/test/integration/InteractionsTest.t.sol
@@ -1,7 +1,24 @@
-// unit 
-// integration 
-// forked 
+// unit
+// integration
+// forked
 // staging <- run tests on a mainnet/testnet
 
-// Todo 
+// Todo
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Raffle} from "../../src/Raffle.sol";
+import {DeployRaffle} from "../../script/DeployRaffle.s.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+
+contract InteractionsTest is Test {
+    Raffle raffle;
+    HelperConfig helperConfig;
+
+    function setUp() external {
+        DeployRaffle deployRaffle = new DeployRaffle();
+        (raffle, helperConfig) = deployRaffle.run();
+    }
+}
