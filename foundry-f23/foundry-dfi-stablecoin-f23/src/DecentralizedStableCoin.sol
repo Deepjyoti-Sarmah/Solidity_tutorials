717fae242e7c607166292e0ea33d6377b5eb80a2
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-08-17 21:53:28 +0530

    decentralized stable coin

---
+++ foundry-f23/foundry-dfi-stablecoin-f23/src/DecentralizedStableCoin.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+/*
+ * @title Decentralized Statble Coin
+ * @author Deepjyoti Sarmah
+ * Colletral: Exogenous (ETH & BTC)
+ * Minting: Algorithmic
+ * Relative Stability: Pegged to USD
+ *
+ * This is a contract meant to be governed by DSCEngine. This contract is just the ERC20 implementation of our stablecoin system.
+ *
+ */
+
+contract DecentralizedStableCoin {
+    constructor() {}
+}
