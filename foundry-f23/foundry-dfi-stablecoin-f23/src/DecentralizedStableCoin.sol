975637106e1b9cb5a15a05857d38da51a2f60195
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-08-18 11:14:16 +0530

    contract is ercburnable and ownable

--- foundry-f23/foundry-dfi-stablecoin-f23/src/DecentralizedStableCoin.sol
+++ foundry-f23/foundry-dfi-stablecoin-f23/src/DecentralizedStableCoin.sol
@@ -1,5 +1,32 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+
+// This is considered an Exogenous, Decentralized, Anchored (pegged), Crypto Collateralized low volitility coin
+
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+pragma solidity 0.8.18;
+
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 /*
  * @title Decentralized Statble Coin
@@ -12,6 +39,7 @@ pragma solidity ^0.8.18;
  *
  */
 
-contract DecentralizedStableCoin {
-    constructor() {}
+contract DecentralizedStableCoin is ERC20Burnable, Ownable {
+    constructor() ERC20("DecentralizedStableCoin", "DSC") {}
+    
 }
