51d32a3b59cdbf615c379caef30b2c9ed93c0199
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-08-22 14:34:08 +0530

    geeting the value of our collateral in usd

--- foundry-f23/foundry-dfi-stablecoin-f23/src/DSCEngine.sol
+++ foundry-f23/foundry-dfi-stablecoin-f23/src/DSCEngine.sol
@@ -28,6 +28,7 @@ pragma solidity 0.8.18;
 import {DecentralizedStableCoin} from "./DecentralizedStableCoin.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 /**
  * @title DSCEngine
@@ -58,9 +59,13 @@ contract DSCEngine is ReentrancyGuard {
     /////////////////////
     // State Variables //
     /////////////////////
+    uint256 private constant ADDITIONAL_FEED_PRECISION = 1e10;
+    uint256 private constant PRECISION = 1e10;
+
     mapping(address token => address priceFeed) private s_priceFeeds;
     mapping(address user => mapping(address token => uint256 amount)) private s_collateralDeposited;
     mapping(address user => uint256 amountDscMinted) private s_DSCMinted;
+    address[] private s_collateralTokens;
 
     DecentralizedStableCoin private immutable i_dsc;
 
@@ -97,6 +102,7 @@ contract DSCEngine is ReentrancyGuard {
         // e.g., ETH/USD, BTC/USD etc
         for (uint256 i = 0; i < tokenAddresses.length; i++) {
             s_priceFeeds[tokenAddresses[i]] = priceFeedAddress[i];
+            s_collateralTokens.push(tokenAddresses[i]);
         }
         i_dsc = DecentralizedStableCoin(dscAddress);
     }
@@ -132,8 +138,8 @@ contract DSCEngine is ReentrancyGuard {
     function redemCollateral() external {}
 
     /*
-     * @param amountDscToMint The amount of decentralized stable coin to mint 
      * @notice follows CEI 
+     * @param amountDscToMint The amount of decentralized stable coin to mint 
      * @notice must have more collatral value than minimum input
      *  
      */
@@ -179,5 +185,21 @@ contract DSCEngine is ReentrancyGuard {
     ////////////////////////////////////////
     // Public and External View Functions //
     ////////////////////////////////////////
-    function getAccountCollateralValue(address user) public view returns (uint256) {}
+    function getAccountCollateralValue(address user) public view returns (uint256 totalCollateralValueInUsd) {
+        //loop through each collateral token, get the amount they have deposited, and get it tp price, to get the USD Value
+        for (uint256 i = 0; i < s_collateralTokens.length; i++) {
+            address token = s_collateralTokens[i];
+            uint256 amount = s_collateralDeposited[user][token];
+            totalCollateralValueInUsd += getUsdValue(token, amount);
+        }
+        return totalCollateralValueInUsd;
+    }
+
+    function getUsdValue(address token, uint256 amount) public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(s_priceFeeds[token]);
+        (, int256 price,,,) = priceFeed.latestRoundData();
+        //1 ETH = $1000
+        // The returned value value from CL will be 1000 * 1e8
+        return (uint256(price) * ADDITIONAL_FEED_PRECISION * amount) / PRECISION;
+    }
 }
