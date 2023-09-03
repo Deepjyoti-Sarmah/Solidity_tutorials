9fc130659d8fe8b9b4e9a7d4f2c25294b5fee7b6
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-09-03 17:12:19 +0530

    testing price feeds

--- foundry-f23/foundry-dfi-stablecoin-f23/test/fuzz/Handler.t.sol
+++ foundry-f23/foundry-dfi-stablecoin-f23/test/fuzz/Handler.t.sol
@@ -7,6 +7,7 @@ import {Test} from "forge-std/Test.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
 import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
 import {ERC20Mock} from "@openzeppelin/contracts/mocks/ERC20Mock.sol";
+import {MockV3Aggregator} from "../mocks/MockV3Aggregator.sol";
 
 contract Handler is Test {
     DSCEngine dsce;
@@ -17,6 +18,7 @@ contract Handler is Test {
 
     uint256 public timesMintIsCalled;
     address[] public usersWithCollateralDeposited;
+    MockV3Aggregator public ethUsdPriceFeed;
 
     uint256 MAX_DEPOSITE_SIZE = type(uint96).max;
 
@@ -27,6 +29,9 @@ contract Handler is Test {
         address[] memory collateralTokens = dsce.getCollateralTokens();
         weth = ERC20Mock(collateralTokens[0]);
         wbtc = ERC20Mock(collateralTokens[1]);
+
+        ethUsdPriceFeed = MockV3Aggregator(dsce.getCollateralTokenPriceFeed(address(weth)));
+
     }
 
     function mintDsc(uint256 amount, uint256 addressSeed) public {
@@ -76,6 +81,12 @@ contract Handler is Test {
         }
         dsce.redemCollateral(address(collateral), amountCollateral);
     }
+    
+    // This brraks our invariant test suite!!!
+    // function updateCollateralPrice(uint96 newPrice) public {
+    //  int256 newPriceInt = int256(uint256(newPrice)); 
+    //  ethUsdPriceFeed.updateAnswer(newPriceInt);
+    // }
 
     //Helper function
     function _getCollateralFromSeed(uint256 collateralSeed) private view returns (ERC20Mock) {
