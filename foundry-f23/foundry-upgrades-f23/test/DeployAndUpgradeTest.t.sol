cabc70cad92146080ac45dfc820f256aa6824f1c
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-09-06 15:57:26 +0530

    adding test to deploy and upgrade

---
+++ foundry-f23/foundry-upgrades-f23/test/DeployAndUpgradeTest.t.sol
@@ -0,0 +1,28 @@
+// // SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+import {DeployBox} from "../script/DeployBox.s.sol";
+import {UpgradeBox} from "../script/UpgradeBox.s.sol";
+import {BoxV1} from "../src/BoxV1.sol";
+import {BoxV2 } from "../src/BoxV2.sol";
+
+contract DeployAndUpgradeTest is Test {
+    DeployBox public deployer;
+    UpgradeBox public upgrader;
+    address public OWNER = makeAddr("owner");
+
+    address public proxy;
+
+    function setUp() public {
+        deployer = new DeployBox();
+        upgrader = new UpgradeBox();        
+        proxy = deployer.run(); // right now points to box v1
+    }
+
+    function testUpgrades() public {
+        BoxV2 box2 = new BoxV2();
+
+    }
+}
