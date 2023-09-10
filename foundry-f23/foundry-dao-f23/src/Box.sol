9bdb7b70618303fbd6e02892989aad22c5b8b421
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-09-10 23:15:38 +0530

    adding forge and openzeppelin

--- foundry-f23/foundry-dao-f23/src/Box.sol
+++ foundry-f23/foundry-dao-f23/src/Box.sol
@@ -2,8 +2,19 @@
 
 pragma solidity ^0.8.18;
 
-contract Box {
-    constructor() {
-        
-    }
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Box is Ownable{
+  uint256 private s_number;
+
+  event NumberChanged(uint256 number);
+
+  function store(uint256 newNumber) public onlyOwner {
+
+    s_number = newNumber;
+    emit NumberChanged(newNumber);
+  
+  }
+
+
 }
