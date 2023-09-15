d881a4e438d815bc7b3d85d42e03b4a1a872d82e
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-09-15 17:15:15 +0530

    adding timelock contract

--- foundry-f23/foundry-dao-f23/src/TimeLock.sol
+++ foundry-f23/foundry-dao-f23/src/TimeLock.sol
@@ -9,5 +9,7 @@ contract TimeLock is TimelockController {
     // minDelay is how long you have to await before executing
     // proposers is the list of addresses that can propose
     // executors is the list of addresses that can be executed
-    constructor(uint256 minDelay) 
+    constructor(uint256 minDelay, address[] memory proposers, address[] memory executers) TimelockController(minDelay, proposers, executers, msg.sender) {
+        
+    } 
 } 
