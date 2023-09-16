cc68f05033e689aab4faa36b70e05117f438559f
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-09-16 12:32:55 +0530

    adding test to a dao

---
+++ foundry-f23/foundry-dao-f23/test/MyGovernorTest.t.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+import {MyGovernor} from "../src/MyGovernor.sol";
+import {Box} from "../src/Box.sol";
+import {GovToken} from "../src/GovToken.sol";
+import {TimeLock} from "../src/TimeLock.sol";
+
+contract MyGovernorTest is Test {
+    function setup() public {
+        
+    }
+}
