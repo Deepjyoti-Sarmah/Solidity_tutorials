e7adf62238d96e1ee64506c76904087898cbcb00
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-09-16 14:15:07 +0530

    testing governance update box

--- foundry-f23/foundry-dao-f23/test/MyGovernorTest.t.sol
+++ foundry-f23/foundry-dao-f23/test/MyGovernorTest.t.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.18;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 import {MyGovernor} from "../src/MyGovernor.sol";
 import {Box} from "../src/Box.sol";
 import {GovToken} from "../src/GovToken.sol";
@@ -19,9 +19,14 @@ contract MyGovernorTest is Test {
 
     address[] proposers;
     address[] executors;
+    
+    uint256[] values;
+    bytes[] calldatas;
+    address[] targets; 
 
     uint256 public constant MINT_DELAY = 3600; //1 hour - after a vote passes
-
+    uint256 public constant VOTING_DELAY = 1; // how many blocks till a vote is active 
+    uint256 public constant VOTING_PERIOD = 50400;
 
     function setup() public {
         govToken = new GovToken();
@@ -49,6 +54,54 @@ contract MyGovernorTest is Test {
     function testCantUpdateBoxWithoutGovernance() public {
         vm.expectRevert();
         box.store(1);
+
+    }
+
+    function testGovernanceUpdatesBox() public {
+        uint256 valueToStore = 888;
+        string memory description = "store 1 in Box";
+        bytes memory encodedFunctionCalls = abi.encodeWithSignature("store(uint256)", valueToStore);
+
+        values.push(0);
+        calldatas.push(encodedFunctionCalls);
+        targets.push(address(box));
+
+        //1. propose to the DAO
+        uint256 proposalId = governor.propose(targets, values, calldatas, description);
+
+        //view the state
+        console.log("Proposal State:", uint256(governor.state(proposalId)));
+
+        vm.warp(block.timestamp + VOTING_DELAY+ 1);     
+        vm.roll(block.number + VOTING_DELAY + 1);
         
+        console.log("Proposal State:", uint256(governor.state(proposalId)));
+
+        //2. Vote 
+        string memory reason = "cuz blue frog is cool";
+
+        uint8 voteWay = 1; //voting yes 
+        vm.prank(USER);
+        governor.castVoteWithReason(proposalId, voteWay, reason);
+
+
+        vm.warp(block.timestamp + VOTING_PERIOD + 1);     
+        vm.roll(block.number + VOTING_PERIOD + 1);
+
+        //3. Queue the TX
+        bytes32 descriptionHash = keccak256(abi.encodePacked(description));
+        governor.queue(targets, values, calldatas, descriptionHash);
+
+        vm.warp(block.timestamp + MINT_DELAY + 1);     
+        vm.roll(block.number + MINT_DELAY + 1);
+
+
+        //4. Execuute 
+        governor.execute(targets, values, calldatas, descriptionHash);
+
+        assert(box.getNumber() == valueToStore);
+        console.log("Box Value:", box.getNumber());
+
+
     }
 }
